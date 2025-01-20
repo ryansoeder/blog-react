006ec0c0dcbddfef041472f794a289477e197cf1
ryansoeder <ryan@mightily.com>
2025-01-20 13:50:45 -0800

    hey, dana

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+	return (
+		<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+			<main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
+				<h1>Hey, Dana.</h1>
+				{/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
@@ -47,9 +48,9 @@ export default function Home() {
           >
             Read our docs
           </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        </div> */}
+			</main>
+			{/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -95,7 +96,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
-    </div>
-  );
+      </footer> */}
+		</div>
+	);
 }
